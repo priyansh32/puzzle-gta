117ae1e8b9aff2cf8d9f4e3860cf97d037547672
Priyansh Patidar <priyanshpatidar.201cs144@nitk.edu.in>
2021-06-10 05:11:35 +0530

    bug fixes, and win function

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const tiles = document.querySelectorAll('.box')
-var color = ["red", "green", "orange", "yellow", "blue", "white"];
-
+const tiles = document.querySelectorAll('.box');
+const youwon = document.querySelector('.winner')
 let emptytile = tiles[24];
 
 const images = [
-    './puzzle/1.png',
-    './puzzle/2.png',
-    './puzzle/3.png',
-    './puzzle/4.png',
-    './puzzle/5.png',
-    './puzzle/6.png',
-    './puzzle/7.png',
-    './puzzle/8.png',
-    './puzzle/9.png',
-    './puzzle/10.png',
-    './puzzle/11.png',
-    './puzzle/12.png',
-    './puzzle/13.png',
-    './puzzle/14.png',
-    './puzzle/15.png',
-    './puzzle/16.png',
-    './puzzle/17.png',
-    './puzzle/18.png',
-    './puzzle/19.png',
-    './puzzle/20.png',
-    './puzzle/21.png',
-    './puzzle/22.png',
-    './puzzle/23.png',
-    './puzzle/24.png'
-]
+    'url("./puzzle/1.png")',
+    'url("./puzzle/2.png")',
+    'url("./puzzle/3.png")',
+    'url("./puzzle/4.png")',
+    'url("./puzzle/5.png")',
+    'url("./puzzle/6.png")',
+    'url("./puzzle/7.png")',
+    'url("./puzzle/8.png")',
+    'url("./puzzle/9.png")',
+    'url("./puzzle/10.png")',
+    'url("./puzzle/11.png")',
+    'url("./puzzle/12.png")',
+    'url("./puzzle/13.png")',
+    'url("./puzzle/14.png")',
+    'url("./puzzle/15.png")',
+    'url("./puzzle/16.png")',
+    'url("./puzzle/17.png")',
+    'url("./puzzle/18.png")',
+    'url("./puzzle/19.png")',
+    'url("./puzzle/20.png")',
+    'url("./puzzle/21.png")',
+    'url("./puzzle/22.png")',
+    'url("./puzzle/23.png")',
+    'url("./puzzle/24.png")',
+    'url("./puzzle/25.png")'
+];
 
 //this array stores list of neighbours for each tile
 const neighbourslist = [
@@ -57,34 +57,59 @@ const neighbourslist = [
     [18, 22, 24],//23
     [19, 23, 25],//24
     [20, 24],//25   
-]
+];
 
+let live = images.slice();
 for (let i = 0; i < 24; i++) {
-    tiles[i].style.backgroundImage = `url(${images[i]})`
+    tiles[i].style.backgroundImage = `${images[i]}`;
 }
-emptytile.style.backgroundImage = 'url("./puzzle/25.png")'
+emptytile.style.backgroundImage = 'url("./puzzle/25.png")';
+
+//this function shuffles the tiles
 
 function shuffle() {
     //even no. of inversions i.e 40
     for (let i = 0; i < 40; i++) {
         //one and two will bw two random tiles that will be inverted
-        let one = Math.floor(Math.random() * images.length);
-        let two = Math.floor(Math.random() * images.length);
-
-        //inverting the background images (ES6 syntax)
-        [tiles[one].style.backgroundImage, tiles[two].style.backgroundImage] = [tiles[two].style.backgroundImage, tiles[one].style.backgroundImage]
+        let one = Math.floor(Math.random() * 24);
+        let two = Math.floor(Math.random() * 24);
+        console.log(one, ' ', two);
+        if (one != two) {
+            //inverting the background images(ES6 syntax)
+            [tiles[one].style.backgroundImage, tiles[two].style.backgroundImage] = [tiles[two].style.backgroundImage, tiles[one].style.backgroundImage];
+            live[one] = tiles[one].style.backgroundImage;
+            live[two] = tiles[two].style.backgroundImage;
+        }
+        else
+            i--;
     }
 }
 
+//this function adds click event listeners to tiles
+function clickEvents() {
+    tiles.forEach((tile, i) => {
+        tile.addEventListener('click', () => {
+            if (neighbourslist[parseInt(emptytile.classList[1]) - 1].includes(parseInt(tile.classList[1]))) {
+                //inverting backgrounds
+                live[parseInt(emptytile.classList[1]) - 1] = tile.style.backgroundImage;
+                [tile.style.backgroundImage, emptytile.style.backgroundImage] = [emptytile.style.backgroundImage, tile.style.backgroundImage];
+                emptytile = tile;
+                live[i] = tile.style.backgroundImage;
+            }
+            if (JSON.stringify(images) == JSON.stringify(live)) {
+                win()
+            }
+        })
+    })
+}
+
+function win() {
+    tiles[0].parentElement.classList.add('disabled')
+    youwon.classList.add('visible')
+}
+
+//shuffling the tiles
 setTimeout(shuffle, 5000)
 
 //adding event listeners to tiles
-tiles.forEach((tile) => {
-    tile.addEventListener('click', () => {
-        if (neighbourslist[parseInt(emptytile.classList[1]) - 1].includes(parseInt(tile.classList[1]))) {
-            //inverting backgrounds
-            [tile.style.backgroundImage, emptytile.style.backgroundImage] = [emptytile.style.backgroundImage, tile.style.backgroundImage]
-            emptytile = tile;
-        }
-    })
-})
+setTimeout(clickEvents, 5000)
