85f190709ac20e177e65f78f6ffb405361fd8198
priyansh32 <patidar.priyansh936@gmail.com>
2023-04-12 06:59:36 +0530

    update

--- src/app/app/play/[id]/page.js
+++ src/app/app/play/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
-import { storage } from "@/lib/storage";
+import { storage } from "@/lib/firebase-client";
 import Grid from "@/components/PuzzleGrid";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, ref } from "firebase/storage";
 export default function Page({ params }) {
   const { id } = params;
@@ -21,9 +21,39 @@ export default function Page({ params }) {
       });
   }, [id]);
 
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const timerStart = useRef(null);
+
+  useEffect(() => {
+    let interval = null;
+    if (timerOn) {
+      timerStart.current = Date.now() / 1000;
+      interval = setInterval(() => {
+        const newTime = Date.now() / 1000 - timerStart.current;
+        setTime(newTime.toFixed(2));
+      }, 10);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerOn, timerStart]);
+
   return (
-    <div className='min-h-content h-full w-full flex flex-col items-center'>
-      {imageGrid && <Grid images={imageGrid} />}
-    </div>
+    <>
+      <div className='min-h-content h-full w-full flex flex-col items-center'>
+        {imageGrid && <Grid images={imageGrid} />}
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          onClick={() => setTimerOn(!timerOn)}
+        >
+          {timerOn ? "Stop" : "Start"}
+        </button>
+
+        <div>
+          <span>{time}</span>
+        </div>
+      </div>
+    </>
   );
 }
