a67975ed3ca0620e4d2782ddd6401d3d7e151acc
Priyansh Patidar <patidar.priyansh936@gmail.com>
2023-07-14 02:59:22 +0530

    updated default link

--- src/app/trynow/page.js
+++ src/app/trynow/page.js
@@ -3,7 +3,7 @@ import Page from '@/app/app/play/[id]/page'
 export default function TryNow() {
   return (
     <Page params={{
-      id: 'qaZXNUtIwYoEWht9XDY4'
+      id: 'HGdrbUs50GT9VvbuwYLo'
     }} />
   );
 }''
